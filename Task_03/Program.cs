b5325363a052ca011e219cb6b2fad8318a7a2a81
Anastasiya <moya_pochta_n.k@mail.ru>
2022-10-06 22:11:36 +0300

    Task_02

---
+++ Task_03/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Введите номер четверти");
+int quarterNumber = int.Parse(Console.ReadLine());
+string range = " ";
+
+if (quarterNumber == 1)
+{
+    Console.WriteLine("Диапазон возможных координат x > 0, y > 0");
+}
+else if (quarterNumber == 2)
+{
+    Console.WriteLine("Диапазон возможных координат x < 0, y > 0");
+}
+else if (quarterNumber == 3)
+{
+     Console.WriteLine("Диапазон возможных координат x < 0, y < 0");
+}
+else
+{
+     Console.WriteLine("Диапазон возможных координат x > 0, y < 0");
+}
