97f5ec0082f010b05eda8dcc8804f7fd5f43b208
Anastasiya <moya_pochta_n.k@mail.ru>
2022-10-06 20:49:18 +0300

    Task_01

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,1 +1,13 @@
 ﻿//Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine ("Введите номер дня недели");
+int x = int.Parse(Console.ReadLine());
+
+if (x < 5)
+{
+    Console.WriteLine("Рабочий");
+}
+else
+{
+    Console.WriteLine("Выходной");
+}
