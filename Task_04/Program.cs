498a4a110791c6b192883f58c75689df374d081d
Anastasiya <moya_pochta_n.k@mail.ru>
2022-10-07 09:09:42 +0300

    Task_04

---
+++ Task_04/Program.cs
@@ -0,0 +1,10 @@
+﻿//Найти расстояние между точками в пространстве 2D
+Console.WriteLine("Введите координаты первой точки x1 и y1");
+double x1 = Convert.ToDouble(Console.ReadLine());
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты первой точки x2 и y2");
+double x2 = Convert.ToDouble(Console.ReadLine());
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Расстояние между точками = " + 
+Math.Round(Match.Sqrt(Match.Pow(Match.Abs(x2-x2), 2) 
++ Match.Pow(Match.Abs(y2-y1), 2)), 2));
